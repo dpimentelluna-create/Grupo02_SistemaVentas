078501dd2b6f55c54c92e7fd5f86a121b4dcc67a
07CHMK1PA111A07 <07CHMK1PA111A07@07CHMK1PA111A07>
2026-04-29 09:43:08 -0500

    Commit 2

--- Grupo02_SistemaVentas/Form1.Designer.cs
+++ Grupo02_SistemaVentas/Form1.Designer.cs
@@ -104,7 +104,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(880, 555);
+            this.ClientSize = new System.Drawing.Size(864, 506);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.checkBox2);
